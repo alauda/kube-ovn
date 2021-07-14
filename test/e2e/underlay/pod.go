b81d88685a7c26e97c3540cb7adf6ab23924dfbb
zhangzujian <zhangzujian.7@gmail.com>
2021-07-14 18:28:15 +0800

    update underlay e2e testing
    1. Add underlay e2e testing for kubectl-ko trace;
    2. Add Pod MTU e2e testing for underlay networking;
    3. Minor fixes.

--- test/e2e/underlay/pod.go
+++ test/e2e/underlay/pod.go
@@ -46,9 +46,6 @@ var _ = Describe("[Underlay Pod]", func() {
 	Expect(cidr).NotTo(BeEmpty())
 	Expect(gateway).NotTo(BeEmpty())
 	Expect(nodeIPs).NotTo(BeEmpty())
-	if len(nodeIPs) < 2 {
-		return
-	}
 
 	namespace := "default"
 	BeforeEach(func() {
@@ -63,56 +60,24 @@ var _ = Describe("[Underlay Pod]", func() {
 				}
 			}
 		}
-	})
-	BeforeEach(func() {
 		if err := f.OvnClientSet.KubeovnV1().Subnets().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
 			if !k8serrors.IsNotFound(err) {
 				klog.Fatalf("failed to delete subnet %s: %v", f.GetName(), err)
 			}
 		}
-	})
-	BeforeEach(func() {
 		if err := f.OvnClientSet.KubeovnV1().Vlans().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
 			if !k8serrors.IsNotFound(err) {
 				klog.Fatalf("failed to delete vlan %s: %v", f.GetName(), err)
 			}
 		}
-	})
-	BeforeEach(func() {
 		if err := f.OvnClientSet.KubeovnV1().ProviderNetworks().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
 			if !k8serrors.IsNotFound(err) {
 				klog.Fatalf("failed to delete provider network %s: %v", f.GetName(), err)
-
 			}
 		}
 		time.Sleep(3 * time.Second)
 	})
 
-	AfterEach(func() {
-		if err := f.OvnClientSet.KubeovnV1().ProviderNetworks().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
-			if !k8serrors.IsNotFound(err) {
-				klog.Fatalf("failed to delete provider network %s, %v", f.GetName(), err)
-
-			}
-		}
-		time.Sleep(3 * time.Second)
-	})
-	AfterEach(func() {
-		if err := f.OvnClientSet.KubeovnV1().Vlans().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
-			if !k8serrors.IsNotFound(err) {
-				klog.Fatalf("failed to delete vlan %s: %v", f.GetName(), err)
-
-			}
-		}
-	})
-	AfterEach(func() {
-		if err := f.OvnClientSet.KubeovnV1().Subnets().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
-			if !k8serrors.IsNotFound(err) {
-				klog.Fatalf("failed to delete subnet %s: %v", f.GetName(), err)
-
-			}
-		}
-	})
 	AfterEach(func() {
 		pods, err := f.KubeClientSet.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: "e2e=true"})
 		if err != nil {
@@ -125,143 +90,284 @@ var _ = Describe("[Underlay Pod]", func() {
 				}
 			}
 		}
+		if err := f.OvnClientSet.KubeovnV1().Subnets().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
+			if !k8serrors.IsNotFound(err) {
+				klog.Fatalf("failed to delete subnet %s: %v", f.GetName(), err)
+			}
+		}
+		if err := f.OvnClientSet.KubeovnV1().Vlans().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
+			if !k8serrors.IsNotFound(err) {
+				klog.Fatalf("failed to delete vlan %s: %v", f.GetName(), err)
+			}
+		}
+		if err := f.OvnClientSet.KubeovnV1().ProviderNetworks().Delete(context.Background(), f.GetName(), metav1.DeleteOptions{}); err != nil {
+			if !k8serrors.IsNotFound(err) {
+				klog.Fatalf("failed to delete provider network %s, %v", f.GetName(), err)
+			}
+		}
+		time.Sleep(3 * time.Second)
 	})
 
-	Describe("Connectivity", func() {
-		It("normal", func() {
-			name := f.GetName()
+	It("mtu", func() {
+		name := f.GetName()
 
-			By("validate node count")
-			nodes, err := f.KubeClientSet.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(len(nodes.Items) > 1).To(BeTrue())
+		By("get cni pod")
+		nodes, err := f.KubeClientSet.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(nodes.Items).NotTo(BeEmpty())
 
-			By("create provider network")
-			pn := kubeovn.ProviderNetwork{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   name,
-					Labels: map[string]string{"e2e": "true"},
-				},
-				Spec: kubeovn.ProviderNetworkSpec{
-					DefaultInterface: providerInterface,
-				},
+		var nodeIP string
+		for _, addr := range nodes.Items[0].Status.Addresses {
+			if addr.Type == corev1.NodeInternalIP {
+				nodeIP = addr.Address
+				break
 			}
-			_, err = f.OvnClientSet.KubeovnV1().ProviderNetworks().Create(context.Background(), &pn, metav1.CreateOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			err = f.WaitProviderNetworkReady(name)
-			Expect(err).NotTo(HaveOccurred())
+		}
+		Expect(nodeIP).NotTo(BeEmpty())
 
-			By("create vlan")
-			vlan := kubeovn.Vlan{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   name,
-					Labels: map[string]string{"e2e": "true"},
-				},
-				Spec: kubeovn.VlanSpec{
-					ID:       0,
-					Provider: pn.Name,
-				},
+		cniPods, err := f.KubeClientSet.CoreV1().Pods("kube-system").List(context.Background(), metav1.ListOptions{LabelSelector: "app=kube-ovn-cni"})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(cniPods).NotTo(BeNil())
+
+		var cniPod *corev1.Pod
+		for _, pod := range cniPods.Items {
+			if pod.Status.HostIP == nodeIP {
+				cniPod = &pod
+				break
 			}
-			_, err = f.OvnClientSet.KubeovnV1().Vlans().Create(context.Background(), &vlan, metav1.CreateOptions{})
-			Expect(err).NotTo(HaveOccurred())
+		}
+		Expect(cniPod).NotTo(BeNil())
 
-			By("create subnet")
-			subnet := kubeovn.Subnet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   name,
-					Labels: map[string]string{"e2e": "true"},
-				},
-				Spec: kubeovn.SubnetSpec{
-					CIDRBlock:       cidr,
-					Gateway:         gateway,
-					ExcludeIps:      append(nodeIPs, gateway),
-					Vlan:            vlan.Name,
-					UnderlayGateway: true,
-				},
-			}
-			_, err = f.OvnClientSet.KubeovnV1().Subnets().Create(context.Background(), &subnet, metav1.CreateOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			err = f.WaitSubnetReady(subnet.Name)
-			Expect(err).NotTo(HaveOccurred())
+		By("change nic mtu")
+		mtu := 1499
+		_, _, err = f.ExecToPodThroughAPI(fmt.Sprintf("ip link set %s mtu %d", providerInterface, mtu), "cni-server", cniPod.Name, cniPod.Namespace, nil)
+		Expect(err).NotTo(HaveOccurred())
 
-			pods := make([]*corev1.Pod, 2)
+		By("create provider network")
+		pn := kubeovn.ProviderNetwork{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.ProviderNetworkSpec{
+				DefaultInterface: providerInterface,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().ProviderNetworks().Create(context.Background(), &pn, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		err = f.WaitProviderNetworkReady(pn.Name)
+		Expect(err).NotTo(HaveOccurred())
 
-			By("create pods")
-			var autoMount bool
-			for i := 0; i < 2; i++ {
-				pods[i] = &corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:        fmt.Sprintf("%s-%d", name, i+1),
-						Namespace:   namespace,
-						Labels:      map[string]string{"e2e": "true"},
-						Annotations: map[string]string{util.LogicalSwitchAnnotation: subnet.Name},
+		By("create vlan")
+		vlan := kubeovn.Vlan{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.VlanSpec{
+				ID:       0,
+				Provider: pn.Name,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().Vlans().Create(context.Background(), &vlan, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create subnet")
+		subnet := kubeovn.Subnet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.SubnetSpec{
+				CIDRBlock:       cidr,
+				Gateway:         gateway,
+				ExcludeIps:      append(nodeIPs, gateway),
+				Vlan:            vlan.Name,
+				UnderlayGateway: true,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().Subnets().Create(context.Background(), &subnet, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		err = f.WaitSubnetReady(subnet.Name)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create pod")
+		var autoMount bool
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        name,
+				Namespace:   namespace,
+				Labels:      map[string]string{"e2e": "true"},
+				Annotations: map[string]string{util.LogicalSwitchAnnotation: subnet.Name},
+			},
+			Spec: corev1.PodSpec{
+				NodeName: nodes.Items[0].Name,
+				Containers: []corev1.Container{
+					{
+						Name:            name,
+						Image:           testImage,
+						ImagePullPolicy: corev1.PullIfNotPresent,
 					},
-					Spec: corev1.PodSpec{
-						NodeName: nodes.Items[i].Name,
-						Containers: []corev1.Container{
-							{
-								Name:            name,
-								Image:           testImage,
-								ImagePullPolicy: corev1.PullIfNotPresent,
-							},
+				},
+				AutomountServiceAccountToken: &autoMount,
+			},
+		}
+		_, err = f.KubeClientSet.CoreV1().Pods(namespace).Create(context.Background(), pod, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		pod, err = f.WaitPodReady(pod.Name, namespace)
+		Expect(err).NotTo(HaveOccurred())
+
+		cmd := fmt.Sprintf("ovs-vsctl --no-heading --columns=external_ids find interface external-ids:pod_name=%s external-ids:pod_namespace=%s", pod.Name, namespace)
+		stdout, _, err := f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
+		Expect(err).NotTo(HaveOccurred())
+		var netns string
+		for _, field := range strings.Fields(stdout) {
+			if strings.HasPrefix(field, "pod_netns=") {
+				netns = strings.TrimPrefix(field, "pod_netns=")
+				netns = netns[:len(netns)-1]
+				break
+			}
+		}
+		Expect(netns).NotTo(BeEmpty())
+
+		cmd = fmt.Sprintf("nsenter --net=%s ip link show eth0", filepath.Join("/var/run/netns", netns))
+		stdout, _, err = f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(stdout).To(ContainSubstring(" mtu %d ", mtu))
+	})
+
+	It("connectivity", func() {
+		if len(nodeIPs) < 2 {
+			return
+		}
+
+		name := f.GetName()
+
+		By("validate node count")
+		nodes, err := f.KubeClientSet.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(nodes.Items) > 1).To(BeTrue())
+
+		By("create provider network")
+		pn := kubeovn.ProviderNetwork{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.ProviderNetworkSpec{
+				DefaultInterface: providerInterface,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().ProviderNetworks().Create(context.Background(), &pn, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		err = f.WaitProviderNetworkReady(pn.Name)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create vlan")
+		vlan := kubeovn.Vlan{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.VlanSpec{
+				ID:       0,
+				Provider: pn.Name,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().Vlans().Create(context.Background(), &vlan, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create subnet")
+		subnet := kubeovn.Subnet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name,
+				Labels: map[string]string{"e2e": "true"},
+			},
+			Spec: kubeovn.SubnetSpec{
+				CIDRBlock:       cidr,
+				Gateway:         gateway,
+				ExcludeIps:      append(nodeIPs, gateway),
+				Vlan:            vlan.Name,
+				UnderlayGateway: true,
+			},
+		}
+		_, err = f.OvnClientSet.KubeovnV1().Subnets().Create(context.Background(), &subnet, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		err = f.WaitSubnetReady(subnet.Name)
+		Expect(err).NotTo(HaveOccurred())
+
+		pods := make([]*corev1.Pod, 2)
+
+		By("create pods")
+		var autoMount bool
+		for i := 0; i < 2; i++ {
+			pods[i] = &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        fmt.Sprintf("%s-%d", name, i+1),
+					Namespace:   namespace,
+					Labels:      map[string]string{"e2e": "true"},
+					Annotations: map[string]string{util.LogicalSwitchAnnotation: subnet.Name},
+				},
+				Spec: corev1.PodSpec{
+					NodeName: nodes.Items[i].Name,
+					Containers: []corev1.Container{
+						{
+							Name:            name,
+							Image:           testImage,
+							ImagePullPolicy: corev1.PullIfNotPresent,
 						},
-						AutomountServiceAccountToken: &autoMount,
 					},
-				}
-				_, err = f.KubeClientSet.CoreV1().Pods(namespace).Create(context.Background(), pods[i], metav1.CreateOptions{})
-				Expect(err).NotTo(HaveOccurred())
-				pods[i], err = f.WaitPodReady(pods[i].Name, namespace)
-				Expect(err).NotTo(HaveOccurred())
+					AutomountServiceAccountToken: &autoMount,
+				},
 			}
-
-			By("get ovs & cni pods")
-			ovsPods, err := f.KubeClientSet.CoreV1().Pods("kube-system").List(context.Background(), metav1.ListOptions{LabelSelector: "app=ovs"})
+			_, err = f.KubeClientSet.CoreV1().Pods(namespace).Create(context.Background(), pods[i], metav1.CreateOptions{})
 			Expect(err).NotTo(HaveOccurred())
-			Expect(ovsPods).NotTo(BeNil())
-			Expect(len(ovsPods.Items)).To(Equal(len(nodes.Items)))
-
-			cniPods, err := f.KubeClientSet.CoreV1().Pods("kube-system").List(context.Background(), metav1.ListOptions{LabelSelector: "app=kube-ovn-cni"})
+			pods[i], err = f.WaitPodReady(pods[i].Name, namespace)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(cniPods).NotTo(BeNil())
-			Expect(len(cniPods.Items)).To(Equal(len(nodes.Items)))
+		}
 
-			for i := 0; i < 2; i++ {
-				var hostIP string
-				for _, addr := range nodes.Items[i].Status.Addresses {
-					if addr.Type == corev1.NodeInternalIP {
-						hostIP = addr.Address
-						break
-					}
+		By("get cni pods")
+		cniPods, err := f.KubeClientSet.CoreV1().Pods("kube-system").List(context.Background(), metav1.ListOptions{LabelSelector: "app=kube-ovn-cni"})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(cniPods).NotTo(BeNil())
+		Expect(len(cniPods.Items)).To(Equal(len(nodes.Items)))
+
+		for i := 0; i < 2; i++ {
+			var hostIP string
+			for _, addr := range nodes.Items[i].Status.Addresses {
+				if addr.Type == corev1.NodeInternalIP {
+					hostIP = addr.Address
+					break
 				}
-				Expect(hostIP).NotTo(BeEmpty())
-
-				var cniPod *corev1.Pod
-				for _, pod := range cniPods.Items {
-					if pod.Status.HostIP == hostIP {
-						cniPod = &pod
-						break
-					}
-				}
-				Expect(cniPod).NotTo(BeNil())
-
-				cmd := fmt.Sprintf("ovs-vsctl --no-heading --columns=external_ids find interface external-ids:pod_name=%s external-ids:pod_namespace=%s", pods[i].Name, namespace)
-				stdout, _, err := f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
-				Expect(err).NotTo(HaveOccurred())
-				var netns string
-				for _, field := range strings.Fields(stdout) {
-					if strings.HasPrefix(field, "pod_netns=") {
-						netns = strings.TrimPrefix(field, "pod_netns=")
-						netns = netns[:len(netns)-1]
-						break
-					}
-				}
-				Expect(netns).NotTo(BeEmpty())
-
-				cmd = fmt.Sprintf("nsenter --net=%s ping -c1 -W1 %s", filepath.Join("/var/run/netns", netns), pods[1-i].Status.PodIP)
-				stdout, _, err = f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(stdout).To(ContainSubstring(" 0% packet loss"))
 			}
-		})
+			Expect(hostIP).NotTo(BeEmpty())
+
+			var cniPod *corev1.Pod
+			for _, pod := range cniPods.Items {
+				if pod.Status.HostIP == hostIP {
+					cniPod = &pod
+					break
+				}
+			}
+			Expect(cniPod).NotTo(BeNil())
+
+			cmd := fmt.Sprintf("ovs-vsctl --no-heading --columns=external_ids find interface external-ids:pod_name=%s external-ids:pod_namespace=%s", pods[i].Name, namespace)
+			stdout, _, err := f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
+			Expect(err).NotTo(HaveOccurred())
+			var netns string
+			for _, field := range strings.Fields(stdout) {
+				if strings.HasPrefix(field, "pod_netns=") {
+					netns = strings.TrimPrefix(field, "pod_netns=")
+					netns = netns[:len(netns)-1]
+					break
+				}
+			}
+			Expect(netns).NotTo(BeEmpty())
+
+			cmd = fmt.Sprintf("nsenter --net=%s ping -c1 -W1 %s", filepath.Join("/var/run/netns", netns), pods[1-i].Status.PodIP)
+			stdout, _, err = f.ExecToPodThroughAPI(cmd, "cni-server", cniPod.Name, cniPod.Namespace, nil)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(stdout).To(ContainSubstring(" 0% packet loss"))
+		}
 	})
 })
